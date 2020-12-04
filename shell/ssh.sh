39b194e78a3904e0842590a5bd9a95daabcee8e5
Ben Alex <ben.alex@acegi.com.au>
2020-12-04 14:38:52 +1100

    Fix SSH login delays

--- shell/ssh.sh
+++ shell/ssh.sh
@@ -9,4 +9,6 @@ chmod 600 /root/.ssh/authorized_keys
 
 echo 'PermitRootLogin yes' >> /etc/ssh/sshd_config
 
+sed -i 's/-account   \[success=1 default=ignore\]  pam_systemd_home.so/# -account   \[success=1 default=ignore\]  pam_systemd_home.so  as per https:\/\/github.com\/systemd\/systemd\/issues\/17266/' /etc/pam.d/system-auth
+
 systemctl enable sshd.service
